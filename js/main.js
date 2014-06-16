b81784c2d03c821711fad470c300eaf41dace930
hrl7 <hrlclb@gmail.com>
2014-06-16 10:37:03 +0900

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+function main(){
+  var scene = new THREE.Scene();
+  var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+  var renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+  geometry = new THREE.CubeGeometry(1,1,1);
+  material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+  cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+  camera.position.z = 5;
+  var render = function () { requestAnimationFrame(render);
+    cube.rotation.x += 0.1;
+    cube.rotation.y += 0.1;
+    renderer.render(scene, camera);
+  };
+  render();
+}
+
+function Joint(){
+
+}
